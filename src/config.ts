9c37484832d3d9bf2799d39c7cd4c2f0560011fd
angeldollface <angeldollface666@gmail.com>
2022-11-10 17:01:28 +0100

    Critical layout changes.

--- src/config.ts
+++ src/config.ts
@@ -1,36 +1,35 @@
 /*
-DOLL FACE by Alexander Abraham, a.k.a. "Angel Dollface".
+DOLL FACE by Alexander Abraham, a.k.a. 'Angel Dollface'.
 Licensed under the MIT license.
 */
 
-export const fiveSevenIcon: string ="/assets/images/site/favicons/apple-icon-57x57.png"
-export const sixZeroIcon: string ="/assets/images/site/favicons/apple-icon-60x60.png"
-export const sevenTwoIcon: string ="/assets/images/site/favicons/apple-icon-72x72.png"
-export const sevenSixIcon: string ="/assets/images/site/favicons/apple-icon-76x76.png"
-export const oneOneFourIcon: string ="/assets/images/site/favicons/apple-icon-114x114.png"
-export const oneTwoZeroIcon: string ="/assets/images/site/favicons/apple-icon-120x120.png"
-export const oneFourFourIcon="/assets/images/favicons/site/apple-icon-144x144.png"
-export const oneFiveTwoIcon: string ="/assets/images/site/favicons/apple-icon-152x152.png"
-export const oneEightZeroIcon: string ="/assets/images/site/favicons/apple-icon-180x180.png"
-export const oneNineTwoIcon: string ="/assets/images/site/favicons/android-icon-192x192.png"
-export const threeTwoIcon: string ="/assets/images/site/favicons/favicon-32x32.png"
-export const nineSixIcon: string ="/assets/images/site/favicons/favicon-96x96.png"
-export const oneSixIcon: string ="/assets/images/site/favicons/favicon-16x16.png"
-export const name: string ="ANGEL DOLLFACE"
-export const description: string ="Everyone\'s favorite doll!"
-export const banner: string ="/assets/images/site/banner/banner.png";
-export const logoPath: string = "/assets/images/site/logo/logo.svg";
-export const menuString: string = "MENU";
-export const closingText: string = "CLOSE";
-export const gaID: string = "";
-export const owner: string = "Alexander Abraham";
-export const homePath: string = "/";
+export const fiveSevenIcon: string ='/assets/images/site/favicons/apple-icon-57x57.png'
+export const sixZeroIcon: string ='/assets/images/site/favicons/apple-icon-60x60.png'
+export const sevenTwoIcon: string ='/assets/images/site/favicons/apple-icon-72x72.png'
+export const sevenSixIcon: string ='/assets/images/site/favicons/apple-icon-76x76.png'
+export const oneOneFourIcon: string ='/assets/images/site/favicons/apple-icon-114x114.png'
+export const oneTwoZeroIcon: string ='/assets/images/site/favicons/apple-icon-120x120.png'
+export const oneFourFourIcon='/assets/images/favicons/site/apple-icon-144x144.png'
+export const oneFiveTwoIcon: string ='/assets/images/site/favicons/apple-icon-152x152.png'
+export const oneEightZeroIcon: string ='/assets/images/site/favicons/apple-icon-180x180.png'
+export const oneNineTwoIcon: string ='/assets/images/site/favicons/android-icon-192x192.png'
+export const threeTwoIcon: string ='/assets/images/site/favicons/favicon-32x32.png'
+export const nineSixIcon: string ='/assets/images/site/favicons/favicon-96x96.png'
+export const oneSixIcon: string ='/assets/images/site/favicons/favicon-16x16.png'
+export const name: string ='ANGEL DOLLFACE'
+export const description: string ='Everyone\'s favorite doll!'
+export const banner: string ='/assets/images/site/banner/banner.png';
+export const logoPath: string = '/assets/images/site/logo/logo.svg';
+export const menuString: string = 'MENU';
+export const closingText: string = 'CLOSE';
+export const owner: string = 'Alexander Abraham';
+export const homePath: string = '/';
 export const navLinks: Object = {
-    'HOME': homePath,
-    'ABOUT': '/pages/about',
-    'ART': '/pages/art',
-    'BLOG': '/pages/blog',
-    'CODING': '/pages/coding'
+    'Home': homePath,
+    'About': '/pages/about',
+    'Art': '/pages/art',
+    'Blog': '/pages/blog',
+    'Coding': '/pages/coding'
 };
 export const footerText: string = 'Angel Dollface Copyright 2022';
 export const socialMediaLinks: Object = {
