75e3fad7b3f8b6dae14687a89c0d996f70ccf4a8
angeldollface <112771657+angeldollface@users.noreply.github.com>
2023-11-17 14:54:00 +0000

    Deploying to gh-pages from @ angeldollface/angeldollface.github.io@3687b6fd898e5968b973358a671e5d5548153f84 🚀

---
+++ assets/javascript/nav.js
@@ -0,0 +1,9 @@
+function openNav(id){
+    let elem = document.getElementById(id);
+    elem.style.width = '100vw';
+}
+
+function closeNav(id){
+    let elem = document.getElementById(id);
+    elem.style.width = '0vw';
+}
