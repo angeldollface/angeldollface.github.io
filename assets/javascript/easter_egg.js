6480fd274fcca6cb3cabc0182e2a562dc786135e
angeldollface <112771657+angeldollface@users.noreply.github.com>
2024-02-10 18:16:22 +0000

    Deploying to gh-pages from @ angeldollface/angeldollface.github.io@de1da6d60b6602a98f1dc5c7ab876b63d56a577d 🚀

---
+++ assets/javascript/easter_egg.js
@@ -0,0 +1,15 @@
+window.easterEggCounter = 0;
+function crackEgg(){
+    window.easterEggCounter = window.easterEggCounter + 1;
+    let easterEggDiv = document.getElementById("easterEgg");
+    if (window.easterEggCounter === 5){
+        easterEggDiv.style.height = '100vh';
+        window.easterEggCounter = 0;
+    }
+    else {}
+}
+function assembleEgg(){
+    let easterEggDiv = document.getElementById("easterEgg");
+    easterEggDiv.style.height = '0vh';
+    
+}
